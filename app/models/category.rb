6829b2024c7b1e3de7a0af21ca42d9ef954049fa
skemtoputaete <mak.goli@yandex.ru>
2017-06-30 17:22:03 +0300

    Added some views

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   self.table_name = 'ms_category'
+  has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   self.table_name = "ms_product"
+  belongs_to :category
 end
