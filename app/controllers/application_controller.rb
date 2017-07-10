1489175a163c8e248bcd62c6d1ca43abc585d2d3
skemtoputaete <mak.goli@yandex.ru>
2017-07-11 00:28:21 +0300

    Changed page after autentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_current_location, :unless => :devise_controller?
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+
+  def after_sign_in_path_for(resource)
+    sign_in_url = new_user_session_url
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
+
+  protected
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:username, :email, :password, :password_confirmation, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:username, :email, :password, :password_confirmation, :current_password, :avatar])
+  end
+  
 end
