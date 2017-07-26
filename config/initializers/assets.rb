da444cc227d65edb14d0162d382ff07e5730eafd
skemtoputaete <mak.goli@yandex.ru>
2017-07-26 13:52:19 +0300

    Changed design of product's description

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,8 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( pagination.css )
+Rails.application.config.assets.precompile += %w( preview-image.css )
+Rails.application.config.assets.precompile += %w( description-image.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
